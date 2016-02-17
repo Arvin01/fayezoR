10c39491feabc58a68fee1b64c20ed806956345f
fayezor <faye.h.zheng@gmail.com>
2016-02-17 18:16:31 -0500

    Test branching

--- R/sim.scRNAseq.R
+++ R/sim.scRNAseq.R
@@ -1,5 +1,7 @@
-#' Simulating scRNA-seq expression data 
-#' Extract parameters from real dataset to simulate scRNA-seq expression data with differentially expressed genes; options for imposing dropouts on the data. 
+### TEST TEST TEST
+
+#' Simulating scRNA-seq expression data
+#' Extract parameters from real dataset to simulate scRNA-seq expression data with differentially expressed genes; options for imposing dropouts on the data.
 #' @param dataset Numeric matrix of read counts from which to extract
 #'   parameters.
 #' @param nreps Desired number of replicates per group.
@@ -14,14 +16,14 @@
 #'   dataset.
 #' @param pDiff Proportion of differentially expressed genes.
 #' @param pUp Proportion of differentially expressed genes that are upregulated.
-#' @param logFC Desired log-fold changes for differentially expressed genes. If single number, all DE genes will exhibit constant logFC. If vector of two numbers provided (default \code{c(0.25, 1.5)}), logFC of DE genes will be drawn uniformly within that range. 
+#' @param logFC Desired log-fold changes for differentially expressed genes. If single number, all DE genes will exhibit constant logFC. If vector of two numbers provided (default \code{c(0.25, 1.5)}), logFC of DE genes will be drawn uniformly within that range.
 #' @param drop.low.lambda Logical, whether to drop low lambdas.
-#' @param drop.extreme.dispersion Proportion of extreme dispersions to drop. 
+#' @param drop.extreme.dispersion Proportion of extreme dispersions to drop.
 #' @param add.dropout Logical, whether to impose dropouts.
 #' @param pDropout Numeric vector of dropout probabilities to consider. Will only be used if \code{add.dropout=TRUE}.
 #' @param drop.method Dropout method, with two options: zero-inflation (default), or low-rate Poisson. Will only be used if \code{add.dropout=TRUE}.
 #' @param drop.lambda Mean of low-rate Poisson for the Poisson-dropout scenario. Will only be used if \code{add.dropout=TRUE} and \code{drop.method="poisson"}.
-#' @param verbose Logical, whether to print simulation progress to screen. 
+#' @param verbose Logical, whether to print simulation progress to screen.
 #' @param seed Optional seed, if it desired to replicate the simulation.
 #' @export
 #' @examples
@@ -29,70 +31,70 @@
 #' # data(prostatedata)
 #' #simData <- sim.scRNAseq(dataset=prostatedata, nreps=3, nTags=20, drop.method=c("zero"))
 #' NULL
-#' @return DGElist object containing all simulation settings and results, including the count matrix and dropout-imposed count matrix.  
+#' @return DGElist object containing all simulation settings and results, including the count matrix and dropout-imposed count matrix.
 
 sim.scRNAseq <- function(dataset, nreps, group=NULL, nTags=NULL, lib.size=NULL, flibs=c(0.7,1.3), pDiff=0.1, pUp=0.5, logFC=c(0.25,1.5), drop.low.lambda=TRUE, drop.extreme.dispersion=0.1, add.dropout=TRUE, pDropout=c(0.1,0.2,0.5,0.8), drop.method=c("zero", "poisson"), drop.lambda=1, verbose=TRUE, seed=NULL){
-  
+
   require(edgeR)
-  
+
   # If group is not provided, set 2 groups with equal number of replicates
   if (is.null(group)){
-    group = as.factor(rep(c("A", "B"), each=nreps)) 
+    group = as.factor(rep(c("A", "B"), each=nreps))
   }
   group = as.factor(group)
   samplenames = paste(group, rep(1:nreps, 2), sep="")
   nlibs = length(group)
   flibs <- rep(flibs, length.out=2)
-  
+
   ##### Preparing the dataset
-  
-  if(verbose) message("Preparing dataset.\n") 
-  
+
+  if(verbose) message("Preparing dataset.\n")
+
   # Sample parameters from dataset
-  dataset <- getDataset(counts = dataset, 
+  dataset <- getDataset(counts = dataset,
                         drop.extreme.dispersion = drop.extreme.dispersion,
                         drop.low.lambda = drop.low.lambda)
-  
+
   # Create new DGEList for storing dataset information
-  dat <- new("DGEList", list(dataset = dataset, 
-                             nTags = nTags, 
-                             lib.size = lib.size, 
-                             flibs = flibs, 
-                             nlibs = nlibs, 
-                             group = group, 
-                             design = model.matrix(~group), 
-                             pDiff= pDiff, 
-                             pUp = pUp)) 
-  
+  dat <- new("DGEList", list(dataset = dataset,
+                             nTags = nTags,
+                             lib.size = lib.size,
+                             flibs = flibs,
+                             nlibs = nlibs,
+                             group = group,
+                             design = model.matrix(~group),
+                             pDiff= pDiff,
+                             pUp = pUp))
+
   ##### Simulate differentially expressed genes
-  
+
   # If lib.size not already specified, sample them randomly from lib sizes sampled from real dataset, with some wiggle room
   if(is.null(lib.size)){
     set.seed(seed)
-    dat$lib.size <- runif(nlibs, 
-                          min = flibs[1]*median(dat$dataset$dataset.lib.size), 
+    dat$lib.size <- runif(nlibs,
+                          min = flibs[1]*median(dat$dataset$dataset.lib.size),
                           max = flibs[2]*median(dat$dataset$dataset.lib.size))
   }
-  
+
   # If nTags set to NULL, take them from real dataset
   if(is.null(nTags)){
     dat$nTags <- dat$dataset$dataset.nTags
   }
-  
+
   # Sampling Lambda and Dispersion parameters to be used in simulated dataset
   # Adds Lambda and Dispersion matrices to dat object
-  if(verbose) message("Sampling.\n")  
-  dat <- sample.fun(dat, seed=seed) 
-  
+  if(verbose) message("Sampling.\n")
+  dat <- sample.fun(dat, seed=seed)
+
   # Alter Lambda's to incorporate differential expression
-  if(verbose) message("Calculating differential expression.\n")	
-  dat <- diff.fun(dat, logFC, seed=seed) 
-  
+  if(verbose) message("Calculating differential expression.\n")
+  dat <- diff.fun(dat, logFC, seed=seed)
+
   # Simulate data using these parameters
-  if(verbose) message("Simulating data.\n")	
+  if(verbose) message("Simulating data.\n")
   dat <- sim.fun(dat, seed=seed)
   colnames(dat$counts) <- samplenames
-  
+
   # Add dropouts
   if(add.dropout){
     if(verbose) message("Adding dropouts.\n")
@@ -100,6 +102,6 @@ sim.scRNAseq <- function(dataset, nreps, group=NULL, nTags=NULL, lib.size=NULL, 
     dat <- dropout.fun(dat, pDropout, drop.method, drop.lambda, seed=seed)
     colnames(dat$counts.dropouts) <- samplenames
   }
-  
+
   dat
 }
